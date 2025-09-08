329dea8c20140226e163e014dafefab2ca54f1e1
Shubham478 <shubhambane435@gmail.com>
2025-09-08 21:27:24 +0530

    setup shadcn and css varibale

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Button } from './components/ui/button'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <h1 className='text-xl text-green-500'>Hello</h1> */}
+    <Button variant="outline">Click Me</Button>
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+
+// Recreate __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+ resolve: {
+  alias: {
+    "@": path.resolve(__dirname, "./src"),
+  },
+},
+
+});
