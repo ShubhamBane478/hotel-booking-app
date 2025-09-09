e712672c06fb1bfb290958abcb6bb6135efc1838
Shubham478 <shubhambane435@gmail.com>
2025-09-09 08:54:12 +0530

    minor header component

--- src/components/layouts/header.layout.tsx
+++ src/components/layouts/header.layout.tsx
@@ -1,13 +1,27 @@
+import { Button } from '@components/ui/button';
+
 const Header = () => {
   return (
     <header className="bg-brand py-2">
       <div className="container flex justify-between items-center">
         <div id="logo-wrapper">
           <a href="#" aria-label="Go to Booking.com">
-            <img src="/assets/booking.com.svg" alt="Logo of Booking.com"/>
+            <img
+              width={144}
+              height={24}
+              src="/assets/booking.com.svg"
+              alt="Logo of Booking.com"
+            />
           </a>
         </div>
-        <div id="auth"></div>
+        <div id="auth" className="flex gap-2 justify-center items-center">
+          <Button className="bg-white cursor-pointer border-primary text-primary rounded-sm hover:bg-white/95">
+            Register
+          </Button>
+          <Button className="bg-white cursor-pointer border-primary text-primary rounded-sm hover:bg-white/95">
+            Login
+          </Button>
+        </div>
       </div>
     </header>
   );
---
+++ src/lib/icon.tsx
@@ -0,0 +1,9 @@
+import { Bed, CarFront, CarTaxiFront, FerrisWheel, Plane } from "lucide-react";
+
+export default {
+    bed: Bed,
+    flight : Plane,
+    car: CarFront,
+    attraction: FerrisWheel,
+    taxi: CarTaxiFront
+}
